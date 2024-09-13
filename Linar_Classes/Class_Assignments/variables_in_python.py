19a317e4ab50e5591a0b1229c6a5c1511e2f1e84
mer-ux <andrewpertual2004@gmail.com>
2024-09-13 14:50:20 -0500

    Update

--- Linar_Classes/Class_Assignments/variables_in_python.py
+++ Linar_Classes/Class_Assignments/variables_in_python.py
@@ -1,4 +1,4 @@
 football_team, baseball_team, basketball_team = "12", "7", "9"
 curly_wigs, straight_wigs, kinky_wigs = "6", "3", "5"
 pink_scarves, blue_ties, black_shoes = "14"
-number_of_laptops, number_of_books, number_of_mangoes = "54"
+number_of_laptops, number_of_books, number_of_mangoes = "545"
